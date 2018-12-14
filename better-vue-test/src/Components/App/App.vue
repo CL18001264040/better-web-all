a4316c0badf4bc9fc8105925639510c666b469a8
better <chenmingyang>
2018-12-14 18:16:27 +0800

    add test html

---
+++ better-vue-test/src/Components/App/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'test'
+            }
+        },
+        computed: {
+            msg() {
+                return this.msg;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
