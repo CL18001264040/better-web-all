2487e8a1a175f92adc676a2b79ff7750d5dd515f
better <chenmingyang>
2018-12-17 17:36:59 +0800

    add todo function component

---
+++ better-vue-cli/src/components/TodoList/CommentList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import Comment from './Comment'
+
+  export default {
+    name: "CommentList",
+    components: {
+      Comment
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ better-vue-cli/src/components/TodoList/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>请发表你对Vue的评论</h2>
+    <AddComment/>
+    <CommentList/>
+  </div>
+</template>
+
+<script>
+  import AddComment from 'AddComment'
+  import CommentList from 'CommentList'
+
+  export default {
+    name: "TodoList",
+    components: {
+      AddComment, CommentList
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
